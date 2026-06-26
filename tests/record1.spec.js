5769988cb281b4b9d410a9c15c0c94afcec6250b
Abhishek Harish <itsjustabhii@gmail.com>
2026-06-26 22:58:19 +0530

    used codegen to generate test cases

---
+++ tests/record1.spec.js
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('record demo test', async ({ page }) => {
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="username"]').press('Tab');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.getByRole('button', { name: 'Open Menu' }).click();
+  await page.locator('[data-test="logout-sidebar-link"]').click();
+});
